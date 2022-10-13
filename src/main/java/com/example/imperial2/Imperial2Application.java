97b2b323226bf19fa75a9de0ed3816376e42087f
Wang <80409907+wanggz1000@users.noreply.github.com>
2022-10-13 02:24:18 +0200

    Initial commit

---
+++ src/main/java/com/example/imperial2/Imperial2Application.java
@@ -0,0 +1,13 @@
+package com.example.imperial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imperial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Imperial2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imperial2/Imperial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imperial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imperial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
